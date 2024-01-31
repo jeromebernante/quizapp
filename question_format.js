9921c4a5291b31c00711bc93662396cbb1d59129
Jerome Bernante <jeromebernante@gmail.com>
2024-01-31 19:12:09 +0800

    amc 1.3

---
+++ question_format.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    question: "question", //INPUT REQUIRE
+    img: "",
+    options: ["", "", "", ""],
+    input: "true",
+    explanation: "because",
+    correctAnswer: "a"
+  },
+  {
+    question: "question",
+    img: "../quiz_images/image.png",  //WITH IMAGES, TRUE & FALSE
+    options: ["TRUE", "FALSE"],
+    input: "false",
+    explanation: "",
+    correctAnswer: "TRUE"
+  },
+  {
+    question: "question", //MULTIPLE CHOICES
+    img: "",
+    options: ["a", "b", "c", "d"],
+    input: "false",
+    explanation: "",
+    correctAnswer: "a"
+  },
+
+];
